4ef3d5930f7d7bf5be73b2a2ec7fda44db65378c
sivakoti5055 <sivakotisrinivas68@gmail.com>
2026-05-01 12:39:03 +0530

    Data Ingestion to Vector DB store Completed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    print("Hello from rag-project!")
+# from langchain_community.document_loaders import PyMuPDFLoader
+
+# loader = PyMuPDFLoader("test.pdf")
+# docs = loader.load()
+
+# print("Loaded documents:", len(docs))
+
+# def main():
+#     print("Hello from rag-project!")
 
 
 if __name__ == "__main__":
